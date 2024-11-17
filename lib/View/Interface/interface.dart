8d6d71eb2344bc57c90ba231631948413e43085f
Moiz <khanmoaiz682@gmail.com>
2024-11-17 18:22:57 +0500

    Interface Updated!

--- lib/View/Interface/interface.dart
+++ lib/View/Interface/interface.dart
@@ -1,4 +1,3 @@
-import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:news_letter/Models/Services/API/api_services.dart';
 import 'package:provider/provider.dart';
